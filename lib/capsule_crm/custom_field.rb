897846730e67f21873d3d4932a6400f6a4f6d193
Rafael Jegundo <rafael.jegundo@gmail.com>
2016-02-24 11:19:52 +0000

    Add number attribute to custom field

--- lib/capsule_crm/custom_field.rb
+++ lib/capsule_crm/custom_field.rb
@@ -27,6 +27,7 @@ module CapsuleCRM
     attribute :tag, String
     attribute :boolean, Boolean
     attribute :text, String
+    attribute :number, Float
 
     validates :id, numericality: { allow_blank: true }
     validates :label, presence: true
